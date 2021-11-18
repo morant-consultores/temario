2cdd4051f1dfd23c3284dc71c37a97adc7bbda21
gorantesj <gorantesj@hotmail.com>
2021-11-18 13:47:12 -0600

    El tema de morant ya está listo

--- R/morant.R
+++ R/morant.R
@@ -4,86 +4,81 @@
 #' @family temas morant
 #' @export
 #' @importFrom ggthemes theme_foundation
-theme_morant <- function(base_size = 18, base_family = "Futura") {
-  print(base_family %in% sysfonts::font_families())
-  if(base_family %in% sysfonts::font_families()){
-    showtext::showtext_auto()
-    colors <- tibble::deframe(temario::colores[["morant"]])
-    (theme_foundation(base_size = base_size,
-                      base_family = base_family)
-      + theme(
-        line = element_line(colour = colors["gris_claro"]),
-        rect = element_rect(fill = "white",
-                            linetype = 0,
-                            colour = NA),
-        text = element_text(colour = colors["gris_medio"]),
-        axis.title = element_blank(),
-        axis.text = element_text(),
-        axis.ticks = element_blank(),
-        axis.line = element_blank(),
-        legend.background = element_rect(),
-        legend.position = "bottom",
-        legend.direction = "horizontal",
-        legend.box = "vertical",
-        panel.grid = element_line(colour = NULL),
-        panel.grid.major.y = element_blank(),
-        panel.grid.major.x =element_line(colour = colors["gris_claro"]),
-        panel.grid.minor = element_blank(),
-        # unfortunately, can't mimic subtitles TODO!
-        plot.title = element_text(hjust = 0, size = rel(1.5), face = "bold"),
-        plot.margin = unit(c(1, 1, 1, 1), "lines"),
-        strip.background = element_rect()))
-
-    # theme_minimal()+
-    #   theme(text = element_text(family = familia, size = tamano_letra),
-    #         plot.title = element_text(size = tamano_titulo,
-    #                                   colour =  color_titulo,
-    #                                   hjust = 0),
-    #         legend.position = "none",
-    #
-    #         panel.grid.major.x = element_line(linetype = ))
-
-  }
-  else print("Instalar font")
+theme_morant <- function(base_size = 12, base_family = "Futura", fondo="#F7F9EC") {
+  colores <- tibble::deframe(temario::colores[["morant"]])
+  (theme_foundation(base_size = base_size,
+                    base_family = base_family)
+    + theme(
+      line = element_line(colour = colores["gris_claro"]),
+      rect = element_rect(fill = fondo,
+                          linetype = 0,
+                          colour = NA),
+      text = element_text(colour = colores["gris_medio"]),
+      axis.title = element_blank(),
+      axis.text = element_text(),
+      axis.ticks = element_blank(),
+      axis.line = element_line(colour = colores["principal"]),
+      legend.background = element_rect(),
+      legend.position = "bottom",
+      legend.direction = "horizontal",
+      legend.box = "vertical",
+      panel.grid = element_line(colour = NULL),
+      panel.grid.major.y = element_blank(),
+      panel.grid.major.x =element_line(colour = colores["gris_claro"],
+                                       linetype = "dotted"),
+      panel.grid.minor = element_blank(),
+      # unfortunately, can't mimic subtitles TODO!
+      plot.title = element_text(hjust = 0,
+                                size = rel(1.5),
+                                face = "bold",
+                                colour = colores["gris_obscuro"]),
+      plot.subtitle = element_text(hjust = 0,
+                                size = rel(1),
+                                face = "bold",
+                                colour = colores["gris_claro"]),
+      plot.margin = unit(c(1, 1, 1, 1), "lines"),
+      strip.text=element_text(colour =colores["terciario"], face = "bold"),
+      strip.background = element_rect(fill=colores["secundario"])))
 }
 
-#' FiveThirtyEight color palette
+#' morant color palette
 #'
-#' The standard three-color FiveThirtyEight palette for line plots comprises
+#' The standard three-color morant palette for line plots comprises
 #' blue, red, and green.
 #'
-#' @family colour fivethirtyeight
+#' @family colour morant
 #' @export
-#' @example inst/examples/ex-fivethirtyeight_pal.R
-fivethirtyeight_pal <- function() {
-  colors <- tibble::deframe(ggthemes::ggthemes_data[["fivethirtyeight"]])
-  values <- unname(colors[c("Blue", "Red", "Green")])
+#' @example inst/examples/ex-morant_pal.R
+morant_pal <- function() {
+  colors <- tibble::deframe(temario::colores[["morant"]] %>%
+                              filter(str_detect(nombre, pattern = "^paleta_")))
+  values <- unname(colors)
   max_n <- length(values)
   f <- manual_pal(values)
   attr(f, "max_n") <- max_n
   f
 }
 
-#' FiveThirtyEight color scales
+#' morant color scales
 #'
-#' Color scales using the colors in the FiveThirtyEight graphics.
+#' Color scales using the colors in the morant graphics.
 #'
 #' @inheritParams ggplot2::scale_colour_hue
-#' @family colour fivethirtyeight
-#' @rdname scale_fivethirtyeight
-#' @seealso \code{\link{theme_fivethirtyeight}()} for examples.
+#' @family colour morant
+#' @rdname scale_morant
+#' @seealso \code{\link{theme_morant}()} for examples.
 #' @export
-scale_colour_fivethirtyeight <- function(...) {
-  discrete_scale("colour", "economist", fivethirtyeight_pal(), ...)
+scale_colour_morant <- function(...) {
+  discrete_scale("colour", "morant", morant_pal(), ...)
 }
 
-#' @rdname scale_fivethirtyeight
+#' @rdname scale_morant
 #' @export
-scale_color_fivethirtyeight <- scale_colour_fivethirtyeight
+scale_color_morant <- scale_colour_morant
 
-#' @rdname scale_fivethirtyeight
+#' @rdname scale_morant
 #' @export
-scale_fill_fivethirtyeight <- function(...) {
-  discrete_scale("fill", "economist", fivethirtyeight_pal(), ...)
+scale_fill_morant <- function(...) {
+  discrete_scale("fill", "morant", morant_pal(), ...)
 }
 
